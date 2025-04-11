7983522ec1d5300a6d8a995cb92bea4aa024e230
12hyeon <hyeonjeong1212@naver.com>
2025-04-11 18:31:10 +0900

    수정

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,27 @@
-// public/service-worker.js
 self.addEventListener('install', (event) => {
-    console.log('서비스 워커 설치 중...');
-    event.waitUntil(self.skipWaiting());
-  });
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/taste.jpg',      // public 폴더에 있는 파일 경로
+      ]).catch((error) => {
+        console.error('캐시 추가 실패:', error);
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
   
   self.addEventListener('activate', (event) => {
     console.log('서비스 워커 활성화됨!');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
   });
   
