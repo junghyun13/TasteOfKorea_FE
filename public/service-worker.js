38edb1b8e093fbfa14a5012f36731317c7d73ed6
12hyeon <hyeonjeong1212@naver.com>
2025-05-01 15:30:06 +0900

    pwa 아이콘 수정

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,8 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/taste.png',      // public 폴더에 있는 파일 경로
+        '/android-chrome-192x192.png',      // public 폴더에 있는 파일 경로
+        '/android-chrome-512x512.png',
         '/manifest.webmanifest'
       ]).catch((error) => {
         console.error('캐시 추가 실패:', error);
@@ -19,8 +20,22 @@ self.addEventListener("fetch", (e) => {
 });
 
   
-  // activate event
+// activate event
 self.addEventListener("activate", (e) => {
-  console.log("[Service Worker] actived", e);
+  console.log("[Service Worker] activated", e);
+  
+  const cacheWhitelist = ['my-cache']; // 새로운 캐시 이름
+
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName); // 오래된 캐시 삭제
+          }
+        })
+      );
+    })
+  );
 });
   
